813837584d1eda4f9665fcbdc82384794aa5452a
maamaazii <mamadrci@gmail.com>
2017-02-25 23:22:02 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,30 +226,17 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {320918622,281621440,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
+    about_text = [[Telemamaz v2 - Open Source
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
+soon
 
 Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
-
-Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+amir
+		yasin
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -513,5 +500,6 @@ end
 -- Start and load values
 our_id = 0
 now = os.time()
-math.randomseed(now)
+math.random
+(now)
 started = false
